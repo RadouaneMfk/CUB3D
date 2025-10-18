35b5651cd632dd1f6869e88c04de11124f5787ed
Hassane-bou <bouchanehassane25@gmail.com>
2025-10-18 09:46:24 +0100

    cub

--- cub3d.c
+++ cub3d.c
@@ -33,6 +33,7 @@ int number_line(char *file)
     close(fd);
     return i;
 }
+
 void print_map(char **file)
 {
     int i = 0;
@@ -43,10 +44,28 @@ void print_map(char **file)
     }
 }
 
+char  *texture_no_space(char *path_no)
+{
+    int i = 0;
+    while(path_no[i])
+        i++;
+    while(path_no[i])
+    {
+        if(path_no[i] == '\n' || path_no[i] == ' ')
+            path_no[i]='\0';
+        i--;
+    }
+    return(path_no);
+}
+
 void parse_texture_line(char **file)
 {
     int i =0;
-    
+    int flag_no = 0;
+    int flag_so = 0;
+    int flag_we = 0;
+    int flag_ea = 0;
+
     char *path_no = NULL;
     char *path_so = NULL;
     char *path_we = NULL;
@@ -55,18 +74,57 @@ void parse_texture_line(char **file)
     while(file[i])
     {
         if(strncmp(file[i],"NO ",3) == 0)
+        {
+            flag_no++;
             path_no =strchr(file[i],'.');
+
+        }
         else if(strncmp(file[i],"SO ",3) == 0)
+        {
+            flag_so++;
             path_so = strchr(file[i],'.');
+        }
         else if(strncmp(file[i],"WE ",3) == 0)
+        {
+            flag_we++;
             path_we = strchr(file[i],'.');
+        }
         else if(strncmp(file[i],"EA ",3) == 0)
+        {
+            flag_ea++;
             path_ea = strchr(file[i],'.');
+        }
         i++;
     }
-    if(!path_no || !path_so || !path_we || !path_ea)
+    if(flag_no != 1 || flag_so != 1 || flag_we != 1 || flag_ea != 1)
+    {
+        printf("duplicate erreur!!!\n");
+        exit(1);
+    }
+    if(!path_no  || !path_so || !path_we || !path_ea)
+    {
         printf("erreur\n");
+        exit(1);
+    }
+    path_no = texture_no_space(path_no);
+    path_so = texture_no_space(path_so);
+    path_we = texture_no_space(path_we);
+    path_ea = texture_no_space(path_ea);
+    printf("---->%s",path_no);
 }
+
+int count_number(char **file)
+{
+    int i = 0;
+    int count = 0;
+    while(file[i])
+    {
+        count++;
+        i++;
+    }
+    return count;
+}
+
 void parse_color_line(char **file)
 {
     int i = 0;
@@ -90,6 +148,11 @@ void parse_color_line(char **file)
             C = ft_split(strchr(file[i],' '),',');
         i++;
     }
+    if(count_number(F) != 3 || count_number(C) != 3)
+    {
+        printf("erreur\n");
+        exit(1);
+    }
     if(F)
     {
         i = 0;
@@ -110,8 +173,15 @@ void parse_color_line(char **file)
         C_r = atoi(C[i++]);
         C_g = atoi(C[i++]);
         C_b = atoi(C[i]);
-        C_color = (C_r << 16) + (C_g << 8) + C_b;
+         if((C_r >= 0 && C_g >= 0 && C_b >=0) && (C_r <= 255 && C_g <= 255 && C_b <= 255)  )    
+            C_color = (C_r << 16) + (C_g << 8) + C_b;
+        else
+        {
+            printf("erreur\n");
+            exit(1);    
+       }    
     }
+    printf("count : %d\n",count_number(F));
     printf("--->F_color: %d\n",F_color);
     printf("--->F_color: %d\n",C_color);
 }
@@ -139,7 +209,7 @@ int main(int ac,char **av)
         }
         map[i] = NULL;
         parse_texture_line(map);
-        parse_color_line(map);
+        parse_color_line(map);  
         // print_map(map);
         close(fd);
         free(map);
