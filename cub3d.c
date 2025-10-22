40f3be1fa470bb36b13d99122bbca0dfcde37a00
Hassane Bouchane <haboucha@c2r4p4.1337.ma>
2025-10-22 10:06:43 +0100

    cub3d_parsing

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: haboucha <haboucha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:44:55 by haboucha          #+#    #+#             */
-/*   Updated: 2025/10/15 15:02:07 by haboucha         ###   ########.fr       */
+/*   Updated: 2025/10/22 09:49:20 by haboucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 int check_extension(char *file)
 {
-    if(strlen(file) < 5 ||  file[strlen(file) - 1] != 'b' || file[strlen(file) - 2] != 'u' ||
+    if(strlen(file) < 5  ||  file[strlen(file) - 1] != 'b' || file[strlen(file) - 2] != 'u' ||
         file[strlen(file) - 3] != 'c'|| file[strlen(file) - 4] != '.' )
         return 0;
     return 1; 
 }
-int number_line(char *file)
+int number_line(char *line)
 {
     int i  = 0;
-    int fd = open(file,O_RDONLY);
-    file = get_next_line(fd);
-    while(file != NULL)
+    int fd = open(line,O_RDONLY);
+    if(fd == -1)
+        exit(1);
+    line = get_next_line(fd);
+    while(line != NULL)
     {
-        file = get_next_line(fd);
         i++;
+        free(line);
+        line = get_next_line(fd);
     }
     // printf("count line= %d\n", i);
     close(fd);
@@ -44,20 +47,49 @@ void print_map(char **file)
     }
 }
 
-char  *texture_no_space(char *path_no)
+void free_split(char **file)
 {
     int i = 0;
-    while(path_no[i])
-        i++;
-    while(path_no[i])
+    while(file[i])
     {
-        if(path_no[i] == '\n' || path_no[i] == ' ')
-            path_no[i]='\0';
-        i--;
+        free(file[i]);
+        i++;
     }
-    return(path_no);
+    free(file);
 }
 
+char  *texture_no_space(char *path)
+{
+    int start = 0;
+    int end = 0;
+    while(isspace(path[start]))
+        start++;
+    end = strlen(path) - 1;
+    while(path[end] && end >= 0)
+    {
+        if(isspace(path[end]))
+            path[end]='\0';
+        end--;
+    }
+    return(path + start);
+}
+int check_path_exist()
+{
+    int fd_no = open("./textures/north.xpm",O_RDONLY);
+    if(fd_no == -1)
+        return(0);
+    // int fd_so = open("./textures/south.xpm",O_RDONLY);
+    // if(fd_so == -1)
+    //     return(0);
+    // int fd_we = open("./textures/west.xpm",O_RDONLY);
+    // if(fd_we == -1)
+    //     return(0);
+    // int fd_ea = open("./textures/eastt.xpm",O_RDONLY);
+    // if(fd_ea == -1)
+    //     return(0);
+    // return (close(fd_no),close(fd_so),close(fd_we),close(fd_ea),1);
+    return(close(fd_no),1);
+}
 void parse_texture_line(char **file)
 {
     int i =0;
@@ -106,13 +138,19 @@ void parse_texture_line(char **file)
         printf("erreur\n");
         exit(1);
     }
+    // printf("avant---->%lu\n",strlen(path_no));
     path_no = texture_no_space(path_no);
+    // printf("apres---->%lu\n",strlen(path_no));
     path_so = texture_no_space(path_so);
     path_we = texture_no_space(path_we);
     path_ea = texture_no_space(path_ea);
-    printf("---->%s",path_no);
+    if(!check_path_exist())
+    {
+        printf("--->Textures file not found\n");
+        exit(1);
+    }
+    // printf("---->%s\n",path_no);
 }
-
 int count_number(char **file)
 {
     int i = 0;
@@ -128,8 +166,10 @@ int count_number(char **file)
 void parse_color_line(char **file)
 {
     int i = 0;
+    
     char **F = NULL;
     char **C = NULL;
+    
     int C_r = 0;
     int C_g = 0;
     int C_b = 0;
@@ -140,13 +180,28 @@ void parse_color_line(char **file)
 
     int C_color = 0;
     int F_color = 0;
+
+    int flag_F = 0;
+    int flag_C =0;
+    
     while(file[i])
     {
         if(strncmp(file[i],"F ",2) == 0)
+        {
             F = ft_split(strchr(file[i],' '),',');
+            flag_F++;
+        }
         else if(strncmp(file[i],"C ",2) == 0)
+        {
             C = ft_split(strchr(file[i],' '),',');
+            flag_C++;
+        }
         i++;
+    }
+    if(flag_F != 1|| flag_C != 1)
+    {
+        printf("Duplicate color definition!!\n");
+        exit(1);
     }
     if(count_number(F) != 3 || count_number(C) != 3)
     {
@@ -156,6 +211,9 @@ void parse_color_line(char **file)
     if(F)
     {
         i = 0;
+        int j = 0;
+        while(isspace(F[i][j]))
+            j++;
         F_r = atoi(F[i++]);
         F_g = atoi(F[i++]);
         F_b = atoi(F[i]);
@@ -164,31 +222,231 @@ void parse_color_line(char **file)
         else
         {
             printf("erreur\n");
+            free_split(F);
             exit(1);    
         }
     }
     if(C)
     {
         i = 0;
+        int j = 0;
+        while(isspace(F[i][j]))
+            j++;
         C_r = atoi(C[i++]);
         C_g = atoi(C[i++]);
         C_b = atoi(C[i]);
-         if((C_r >= 0 && C_g >= 0 && C_b >=0) && (C_r <= 255 && C_g <= 255 && C_b <= 255)  )    
+        if((C_r >= 0 && C_g >= 0 && C_b >=0) && (C_r <= 255 && C_g <= 255 && C_b <= 255)  )    
             C_color = (C_r << 16) + (C_g << 8) + C_b;
         else
         {
             printf("erreur\n");
+            free_split(C);
             exit(1);    
        }    
     }
-    printf("count : %d\n",count_number(F));
-    printf("--->F_color: %d\n",F_color);
-    printf("--->F_color: %d\n",C_color);
+    free_split(F);
+    free_split(C);
 }
 
+int find_start_of_map(char **file)
+{
+    int i = 0;
+    while(file[i])
+    {
+        int j = 0;
+        while(file[i][j] && isspace(file[i][j]))
+            j++;
+        if(file[i][j] == '\0')
+        {
+            i++;
+            continue;
+        }
+        if(strncmp(file[i],"NO ",3) == 0 || strncmp(file[i],"SO ",3) == 0 ||
+            strncmp(file[i],"WE ",3) == 0 || strncmp(file[i],"EA ",3) == 0 ||  
+            strncmp(file[i],"F ",2) == 0 || strncmp(file[i],"C ",2) == 0) 
+            {
+                i++;
+                continue;
+            }
+        int start = 1;
+        j =0;
+        while(file[i][j])
+        {
+            if(file[i][j] != ' ' && file[i][j] != '0'  && file[i][j] != '1' &&
+            file[i][j] != 'S' &&  file[i][j] != 'N' && file[i][j] != 'W' && file[i][j] != 'E'  && file[i][j] != '\n'  )
+            {
+                start = 0;
+                break;
+            }
+        j++;   
+        }
+        if(start)
+            return(i);
+        i++;
+    }
+    return (-1);
+}
+int count_line(char **map,int i)
+{
+    int count = 0;
+    while(map[i])
+    {
+        i++;
+        count++;
+    }
+    return(count);
+}
+int element_valid(char **file)
+{
+    int i = 0;
+    int j;
+    while(file[i])
+    {
+        j = 0;
+        while(file[i][j])
+        {
+            if(file[i][j] != '0'  && file[i][j] != '\n' && file[i][j] != '1' && file[i][j] != ' ' && file[i][j] != 'N' && file[i][j] !='S' && file[i][j] != 'E' && file[i][j] != 'W')
+            {
+                return 0;
+            }
+            j++;
+        }
+        i++;
+    }
+    return 1;
+    
+}
+char  check_palyer(char **map)
+{
+    int count_player = 0;
+    char player_dir;
+    int i =0;
+    int j;
+    while(map[i])
+    {
+        j = 0;
+        while(map[i][j])
+        {
+            if(map[i][j] == 'W' || map[i][j] == 'A' || map[i][j] == 'S'
+                || map[i][j] == 'E')
+                {
+                    count_player++;
+                    player_dir = map[i][j];
+                }
+            j++;
+        }
+        i++;
+    }
+    if(count_player > 1)
+    {
+        printf("multiple player in the map!!\n");
+        exit(1);        
+    }
+    else if(count_player == 0)
+    {
+        printf("player not found!!\n");
+        exit(1);
+    }
+    return(player_dir);    
+}
+
+int check_first_line(char **map)
+{
+    int i = 0;
+    while(map[0][i])
+    {
+        if(map[0][i] == '0' || map[0][i] == 'N' || map[0][i] == 'S' || map[0][i] == 'W' || map[0][i] == 'E')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+int check_last_line(char **map)
+{
+    int last_line = count_line(map,0);
+    last_line = last_line - 1;
+    int i = 0;
+    while(map[last_line][i])
+    {
+        if(map[last_line][i] == '0' || map[last_line][i] == 'N' || map[last_line][i] == 'S' 
+            || map[last_line][i] == 'W' || map[last_line][i] == 'E')
+            return 0;
+        i++;
+    }
+    return 1;
+    
+}
+
+int valid_walls(char **map)
+{
+    if(check_first_line(map) ==  0)
+        return 0;
+    else if(check_last_line(map) == 0)
+       return 0;
+    return 1;
+}
+int check_first_char(char **map)
+{
+    int i = 1;
+    int j;
+    while(map[i])
+    {
+        j = 0;
+        while(isspace(map[i][j]))
+            j++;
+        if(map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+int check_last_char(char **map)
+{
+    int i = 1;
+    int j = 0;
+    while(map[i])
+    {
+        j = strlen(map[i]) - 2;
+        while(isspace(map[i][j]))
+            j--;
+        if(map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W')
+            return 0;
+        i++;
+    }  
+    return 1;
+}
+
+int valid_map(char **map)
+{
+    int i = 0;
+    int j;
+    while(map[i])
+    {
+        j= 0;
+        while(map[i][j])
+        {
+            if(map[i][j] == '0' || map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E'
+                || map[i][j] == 'W')
+                {
+                    if(j  > 0 && map[i][j-1] == ' ')
+                        return 0;
+                    if(map[i][j+1] == ' ')
+                        return 0;
+                    if(map[i+1] != NULL && map[i+1][j] == ' ' )
+                        return 0;
+                    if( i > 0  && map[i - 1] != NULL && map[i - 1][j] == ' ')
+                        return 0;
+                }
+            j++;
+        }
+        i++;
+    }
+    return 1;
+}
 int main(int ac,char **av)
 {
     char **map = NULL;
+    char **new_map = NULL;
     int count;
     if(ac == 2)
     {
@@ -209,8 +467,55 @@ int main(int ac,char **av)
         }
         map[i] = NULL;
         parse_texture_line(map);
-        parse_color_line(map);  
-        // print_map(map);
+        parse_color_line(map);
+        int start = find_start_of_map(map);
+        int count_new_map = count_line(map,start);
+        if(start == -1)
+        {
+            printf("map invalide!!!\n");
+            exit(1);
+        }
+        // else
+        // {
+        //     // printf("---->%d\n",start);
+        // }
+        new_map = malloc(sizeof(char *) * (count_new_map + 1));
+        int begin = 0;
+        while(map[start])
+        {
+            new_map[begin] = strdup(map[start]);
+            start++;
+            begin++;
+        }
+        new_map[begin] = NULL;
+        if(element_valid(new_map) == 0)
+        {
+            printf("element no valid in map !!\n");
+            exit(1);    
+        }
+        char player_dir = check_palyer(new_map);
+        printf("--->pos_plyer: %c\n",player_dir);
+        if(valid_walls(new_map) == 0)
+        {
+            printf("check valadition map!!\n");
+            exit(1);
+        }
+        if(check_first_char(new_map) == 0)
+        {
+            printf("fixe first char\n");
+            exit(1);
+        }
+        if(check_last_char(new_map) == 0)
+        {
+            printf("fixe last char\n");
+            exit(1);
+        }
+        if(valid_map(new_map) == 0)
+        {
+            printf("le map invalid!!\n");
+            exit(1);
+        }
+        print_map(new_map);
         close(fd);
         free(map);
         return 0;
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: haboucha <haboucha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:43:57 by haboucha          #+#    #+#             */
-/*   Updated: 2025/10/14 12:54:38 by haboucha         ###   ########.fr       */
+/*   Updated: 2025/10/18 12:08:04 by haboucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 #include "./get_next_line/get_next_line.h"
 
 char	**ft_split(char const *s, char c);
